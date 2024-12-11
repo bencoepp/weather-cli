368678e00efb20557539645fe3459b95ea1bb637
Ben Cöppicus <bencoepp@gmail.com>
2024-12-11 13:29:09 +0100

    Add SQLiteHandler for database interaction
    Introduce `SQLiteHandler` to manage database operations, including CRUD functionalities for `Measurement` and `Station` entities. Updated `CMakeLists.txt` to include the new files and link `SQLiteCpp` library. This enhances database handling and improves modularity.

---
+++ SQLiteHandler.h
@@ -0,0 +1,35 @@
+﻿#ifndef SQLITEHANDLER_H
+#define SQLITEHANDLER_H
+#include <string>
+
+#include "Measurement.h"
+#include "Station.h"
+#include "SQLiteCpp/Database.h"
+
+
+class SQLiteHandler {
+public:
+    explicit SQLiteHandler(std::string database);
+    void init();
+    Measurement getMeasurement(std::string stationId);
+    Station getStation(std::string stationId);
+    Measurement& insertMeasurement(Measurement& measurement);
+    Station& insertStation(Station& station);
+    Measurement& updateMeasurements(Measurement& measurement);
+    Station& updateStation(Station& station);
+    bool deleteMeasurements(Measurement& measurement);
+    bool deleteStation(Station& station);
+    bool cleanDatabase();
+    std::vector<Measurement> getAllMeasurements();
+    std::vector<Station> getAllStations();
+    int countMeasurements();
+    int countStations();
+    ~SQLiteHandler();
+private:
+    std::string database;
+    SQLite::Database db;
+};
+
+
+
+#endif //SQLITEHANDLER_H
